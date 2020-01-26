bdc082c90a85e07457dfb7b1a1f5b8a7b623530a
Faraz Sanal <farazsanal@nichols-ve703-12252.apn.wlan.private.upenn.edu>
2020-01-26 14:33:39 -0500

    finished all relevant reducers + actions below cut off

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,10 +2,13 @@ import { combineReducers } from 'redux';
 import documentReducer from './Document_Reducer'
 import snippetReducer from './Snippet_Reducer';
 import requestReducer from './Request_Reducer';
+import commentReducer from './Comment_Reducer';
+
 
 export default combineReducers({
     documentState: documentReducer,
     snippetState: snippetReducer,
-    requestReducer: requestReducer
+    requestState: requestReducer,
+    commentState: commentReducer
 });
 
