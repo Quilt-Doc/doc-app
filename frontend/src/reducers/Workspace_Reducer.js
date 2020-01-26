102c5228e6fcccdbda4d2f829417ba3a85497ecc
Karan Godara <kxg3442@rit.edu>
2020-01-26 14:18:30 -0500

    Workspace Reducer Initial

--- frontend/src/reducers/Workspace_Reducer.js
+++ frontend/src/reducers/Workspace_Reducer.js
@@ -7,3 +7,21 @@ import {
 } from '../actions/types/Workspace_Types'
 
 import _ from 'lodash';
+
+
+export default (state = {}, action) => {
+    switch (action.type) {
+        case CREATE_WORKSPACE:
+            return { ...state, [action.payload._id]: action.payload };
+        case GET_WORKSPACE:
+            return { ...state, [action.payload._id]: action.payload };
+        case DELETE_WORKSPACE:
+            return _.omit(state, action.payload._id);
+        case WORKSPACE_ADD_USER:
+            return { ...state, [action.payload._id]: action.payload };
+        case WORKSPACE_REMOVE_USER:
+            return { ...state, [action.payload._id]: action.payload };
+        default: 
+            return state;
+    }
+}
