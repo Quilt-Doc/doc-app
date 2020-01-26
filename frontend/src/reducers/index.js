e6ec6a01d4d63e1f39647d2670f83b23824687a0
Faraz Sanal <farazsanal@nichols-ve703-12252.apn.wlan.private.upenn.edu>
2020-01-26 14:37:43 -0500

    added reducers to combine_reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -3,12 +3,15 @@ import documentReducer from './Document_Reducer'
 import snippetReducer from './Snippet_Reducer';
 import requestReducer from './Request_Reducer';
 import commentReducer from './Comment_Reducer';
-
+import userReducer from './User_Reducer'
+import tagReducer from './Tag_Reducer';
 
 export default combineReducers({
-    documentState: documentReducer,
-    snippetState: snippetReducer,
-    requestState: requestReducer,
-    commentState: commentReducer
+    documents: documentReducer,
+    snippets: snippetReducer,
+    requests: requestReducer,
+    comments: commentReducer,
+    users: userReducer,
+    tags: tagReducer,
 });
 
