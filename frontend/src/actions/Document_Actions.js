4bc3ea2d7a9b4e3fe2f41c7e24168055bf05ac17
Faraz Sanal <farazsanal@nichols-ve703-12252.apn.wlan.private.upenn.edu>
2020-01-26 00:28:44 -0500

    Doc Reducer Partway

--- frontend/src/actions/Document_Actions.js
+++ frontend/src/actions/Document_Actions.js
@@ -1,14 +1,108 @@
 import {
-    CREATE_DOCUMENT
+    CREATE_DOCUMENT,
+    GET_DOCUMENT,
+    RETRIEVE_DOCUMENTS,
+    DELETE_DOCUMENT,
+    EDIT_DOCUMENT,
+    ATTACH_DOCUMENT_TAG,
+    REMOVE_DOCUMENT_TAG,
+    ATTACH_DOCUMENT_SNIPPET,
+    REMOVE_DOCUMENT_SNIPPET,
+    ATTACH_DOCUMENT_PARENT,
+    REMOVE_DOCUMENT_PARENT,
+    ATTACH_DOCUMENT_UPLOADFILE,
+    REMOVE_DOCUMENT_UPLOADFILE,
+    ADD_DOCUMENT_CANWRITE,
+    REMOVE_DOCUMENT_CANWRITE,
+    ADD_DOCUMENT_CANREAD,
+    REMOVE_DOCUMENT_CANREAD
 } from './types/Document_Types';
 
 import api from '../apis/api';
 
- 
-export const createDocument = (workspaceID, formValues) => async (dispatch, getState) => {
-    let authorID = getState()
-    const response = await api.post('/documents/create', {...formValues, workspaceID});
+
+export const createDocument = (formValues) => async (dispatch) => {
+    const response = await api.post('/documents/create', { ...formValues });
     dispatch({ type: CREATE_DOCUMENT, payload: response.data });
 }
 
+export const getDocument = id => async dispatch => {
+    const response = await api.get(`/documents/get/${id}`);
+    dispatch({ type: GET_DOCUMENT, payload: response.data });
+}
 
+export const retrieveDocuments = (formValues) => async dispatch => {
+    const response = await api.post(`/documents/retrieve`, { ...formValues });
+    dispatch({ type: RETRIEVE_DOCUMENTS, payload: response.data });
+}
+
+export const deleteDocument = id => async dispatch => {
+    const response = await api.delete(`/documents/delete/${id}`);
+    dispatch({ type: DELETE_DOCUMENT, payload: response.data });
+}
+
+export const editDocument = (id, formValues) => async dispatch => {
+    const response = await api.put(`/documents/edit/${id}`, formValues);
+    dispatch({ type: EDIT_DOCUMENT, payload: response.data });
+}
+
+export const attachDocumentTag = (id, tagID) => async (dispatch) => {
+    const response = await api.put(`/documents/attach_tag/${id}`, { tagID });
+    dispatch({ type: ATTACH_DOCUMENT_TAG, payload: response.data });
+}
+
+export const removeDocumentTag = (id, tagID) => async (dispatch) => {
+    const response = await api.put(`/documents/remove_tag/${id}`, { tagID });
+    dispatch({ type: REMOVE_DOCUMENT_TAG, payload: response.data });
+}
+
+
+export const attachDocumentSnippet = (id, snippetID) => async (dispatch) => {
+    const response = await api.put(`/documents/attach_snippet/${id}`, { snippetID });
+    dispatch({ type: ATTACH_DOCUMENT_SNIPPET, payload: response.data });
+}
+
+export const removeDocumentSnippet = (id, snippetID) => async (dispatch) => {
+    const response = await api.put(`/documents/remove_snippet/${id}`, { snippetID });
+    dispatch({ type: REMOVE_DOCUMENT_SNIPPET, payload: response.data });
+}
+
+export const attachDocumentParent = (id, parentID) => async (dispatch) => {
+    const response = await api.put(`/documents/attach_parent/${id}`, { parentID });
+    dispatch({ type: ATTACH_DOCUMENT_PARENT, payload: response.data });
+}
+
+export const removeDocumentParent = (id, parentID) => async (dispatch) => {
+    const response = await api.put(`/documents/remove_parent/${id}`, { parentID });
+    dispatch({ type: REMOVE_DOCUMENT_PARENT, payload: response.data });
+}
+
+export const attachDocumentUploadFile = (id, uploadFileID) => async (dispatch) => {
+    const response = await api.put(`/documents/attach_uploadfile/${id}`, { uploadFileID });
+    dispatch({ type: ATTACH_DOCUMENT_UPLOADFILE, payload: response.data });
+}
+
+export const removeDocumentUploadFile = (id, uploadFileID) => async (dispatch) => {
+    const response = await api.put(`/documents/remove_uploadfile/${id}`, { uploadFileID });
+    dispatch({ type: REMOVE_DOCUMENT_UPLOADFILE, payload: response.data });
+}
+
+export const addDocumentCanWrite = (id, userID) => async (dispatch) => {
+    const response = await api.put(`/documents/add_canwrite/${id}`, { userID });
+    dispatch({ type: ADD_DOCUMENT_CANWRITE, payload: response.data });
+}
+
+export const removeDocumentCanWrite = (id, userID) => async (dispatch) => {
+    const response = await api.put(`/documents/remove_canwrite/${id}`, { userID });
+    dispatch({ type: REMOVE_DOCUMENT_CANWRITE, payload: response.data });
+}
+
+export const addDocumentCanRead = (id, userID) => async (dispatch) => {
+    const response = await api.put(`/documents/add_canread/${id}`, { userID });
+    dispatch({ type: ADD_DOCUMENT_CANREAD, payload: response.data });
+}
+
+export const removeDocumentCanRead = (id, userID) => async (dispatch) => {
+    const response = await api.put(`/documents/remove_canread/${id}`, { userID });
+    dispatch({ type: REMOVE_DOCUMENT_CANREAD, payload: response.data });
+}
--- frontend/src/reducers/Document_Reducer.js
+++ frontend/src/reducers/Document_Reducer.js
@@ -1,5 +1,9 @@
 import {
-    CREATE_DOCUMENT
+    CREATE_DOCUMENT, GET_DOCUMENT, RETRIEVE_DOCUMENTS, DELETE_DOCUMENT, 
+    EDIT_DOCUMENT, ATTACH_DOCUMENT_TAG, REMOVE_DOCUMENT_TAG, ATTACH_DOCUMENT_SNIPPET, 
+    REMOVE_DOCUMENT_SNIPPET, REMOVE_DOCUMENT_PARENT, ATTACH_DOCUMENT_UPLOADFILE, 
+    REMOVE_DOCUMENT_UPLOADFILE, ADD_DOCUMENT_CANWRITE, REMOVE_DOCUMENT_CANWRITE, 
+    ADD_DOCUMENT_CANREAD, REMOVE_DOCUMENT_CANREAD
 } from '../actions/types/Document_Types'
 
 import _ from 'lodash';
@@ -8,7 +12,27 @@ import _ from 'lodash';
 export default (state = {}, action) => {
     switch (action.type) {
         case CREATE_DOCUMENT:
-            return {...state, [action.payload._id]: action.payload}
+            return { ...state, [action.payload._id]: action.payload };
+        case GET_DOCUMENT:
+            return { ...state, [action.payload._id]: action.payload };
+        case RETRIEVE_DOCUMENTS:
+            return { ..._.mapKeys(action.payload, '_id') };
+        case DELETE_DOCUMENT:
+            return _.omit(state, action.payload._id);
+        case EDIT_DOCUMENT:
+            return { ...state, [action.payload._id]: action.payload };
+        case ATTACH_DOCUMENT_TAG:
+        case REMOVE_DOCUMENT_TAG:
+        case ATTACH_DOCUMENT_SNIPPET:
+        case REMOVE_DOCUMENT_SNIPPET:
+        case ATTACH_DOCUMENT_PARENT:
+        case REMOVE_DOCUMENT_PARENT:
+        case ATTACH_DOCUMENT_UPLOADFILE:
+        case REMOVE_DOCUMENT_UPLOADFILE:
+        case ADD_DOCUMENT_CANWRITE:
+        case REMOVE_DOCUMENT_CANWRITE:
+        case ADD_DOCUMENT_CANREAD:
+        case REMOVE_DOCUMENT_CANREAD:
         default: 
             return state;
     }
