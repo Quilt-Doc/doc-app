0e8f88e466816ef44c30f9568b4f301901c0b280
Karan Godara <kxg3442@rit.edu>
2020-01-26 14:50:32 -0500

    Combine Reducers

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,22 +1,25 @@
 import { combineReducers } from 'redux';
-import documentReducer from './Document_Reducer'
+
+import workspaceReducer from './Workspace_Reducer';
+import codebaseReducer from './Codebase_Reducer';
+import folderReducer from './Folder_Reducer';
+
+import documentReducer from './Document_Reducer';
 import snippetReducer from './Snippet_Reducer';
 import requestReducer from './Request_Reducer';
 import commentReducer from './Comment_Reducer';
-import userReducer from './User_Reducer'
+import userReducer from './User_Reducer';
 import tagReducer from './Tag_Reducer';
 
 export default combineReducers({
+    workspaces: workspaceReducer,
+    codebases: codebaseReducer,
+    folders: folderReducer,
     documents: documentReducer,
-<<<<<<< HEAD
-    snippetState: snippetReducer,
-    requestReducer: requestReducer
-=======
     snippets: snippetReducer,
     requests: requestReducer,
     comments: commentReducer,
     users: userReducer,
     tags: tagReducer,
->>>>>>> e6ec6a01d4d63e1f39647d2670f83b23824687a0
 });
 
