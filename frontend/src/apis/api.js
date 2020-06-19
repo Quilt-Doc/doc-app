d0d90bd74d681791ec7c840bd43e91c5cc05fd07
Kgodara <kxg3442@rit.edu>
2020-06-19 02:41:03 -0400

    Fix Merge Error

--- frontend/src/apis/api.js
+++ frontend/src/apis/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-<<<<<<< HEAD
 /*export default axios.create({
     baseURL: "http://localhost:3001/api"
 });*/
@@ -8,17 +7,3 @@ import axios from 'axios';
 export default axios.create({
     baseURL: "http://54.160.81.133:3001/api"
 });
->>>>>>> 57723ca9a00a701b0554ed83f2acd12fed9af3fc
-
-export default axios.create({
-    baseURL: "http://54.160.81.133:3001/api"
-<<<<<<< HEAD
-});
-=======
-});*/
-
-
-/*
-baseURL: "http://localhost:3001/api"
-*/
->>>>>>> 57723ca9a00a701b0554ed83f2acd12fed9af3fc
