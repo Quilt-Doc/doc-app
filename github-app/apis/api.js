23d28345d1981b4107737fbb62946886452b3e95
kgodara <kxg3442@rit.edu>
2020-06-27 18:58:09 +0000

    updated listening

--- github-app/apis/api.js
+++ github-app/apis/api.js
@@ -1,11 +1,50 @@
-const axios = require("axios");
+const requestClient = () => {
+    const axios = require('axios');
+    return axios.create({
+        baseURL: "https://api.github.com",
+        headers: {
+            post: {        // can be common or any other method
+                Authorization: 'token ' + process.env.OAUTH_TOKEN
+            },
+            get: {        // can be common or any other method
+                Authorization: 'token ' + process.env.OAUTH_TOKEN
+              }
+          }
+    });
+}
 
-var api = axios.create({
-    baseURL: "http://54.160.81.133:3001/api"
-});
+const requestBackendClient = () => {
+	const axios = require('axios');
+	return axios.create({
+		baseURL: "http://54.160.81.133:3001/api"
+	})
+}
 
-module.exports = { api };
+const requestSQSServiceObject = () => {
+    // Load the AWS SDK for Node.js
+    var AWS = require('aws-sdk');
+    // Set the region 
+    AWS.config.update({region: 'us-east-1'});
 
-/*export default axios.create({
-    baseURL: "http://54.160.81.133:3001/api"
-});*/
+    // Create an SQS service object
+    var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+
+    /*var params = {
+      QueueName: 'dataUpdate.fifo'
+    };
+
+    sqs.getQueueUrl(params, function(err, data) {
+        if (err) {
+            console.log("Error", err);
+        } else {
+            console.log("Success", data.QueueUrl);
+        }
+    });*/
+    return sqs;
+}
+
+module.exports = {
+    requestClient,
+    requestBackendClient,
+    requestSQSServiceObject
+}
