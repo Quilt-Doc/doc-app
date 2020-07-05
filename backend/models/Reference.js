f04c7f7e46b51a4e0146d63d7685e348b1fe5135
kgodara <kxg3442@rit.edu>
2020-07-05 08:59:14 +0000

    added description field

--- backend/models/Reference.js
+++ backend/models/Reference.js
@@ -2,11 +2,15 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema;
 const { ObjectId, Mixed } = Schema.Types;
 
+
 var referenceSchema = new Schema({
 	name: String,
 	kind: String,
 	path: String,
+	description: String,
 	lineNum: Number,
+	position: String,
+	parseProvider: String,
 	repository: {type: ObjectId, ref: 'Repository'},
 	created: {type: Date, default: Date.now }
 });
