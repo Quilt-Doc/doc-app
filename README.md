0944ef7e0e9bbe8b989b8caf0490e63370105a06
Karan Godara <kxg3442@rit.edu>
2020-08-22 05:53:09 -0400

    Removed garbage

--- README.md
+++
@@ -1,2 +0,0 @@
-# DocApp
-A product to help coders document their software on the fly
--- frontend/src/apis/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-var apiEndpoint = process.env.REACT_APP_LOCAL_URL;
-
-var api = axios.create({
-    baseURL: apiEndpoint
-});
-
-export {api, apiEndpoint}
