fbfd79f560b50ac4f968bfa1d9cbe6ff8156881b
Karan Godara <kxg3442@rit.edu>
2020-09-05 22:52:57 -0400

    added dev token

--- github-app/apis/api.js
+++ github-app/apis/api.js
@@ -1,7 +1,10 @@
 const requestBackendClient = () => {
 	const axios = require('axios');
 	return axios.create({
-		baseURL: process.env.BACKEND_API_URL
+        baseURL: process.env.BACKEND_API_URL,
+        headers: {
+            "Authorization": `Bearer ${process.env.DEV_TOKEN}`
+        }
 	})
 }
 
