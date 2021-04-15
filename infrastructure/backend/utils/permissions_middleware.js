434fa5e07d973338d80d941e0dec321fbb70b180
fsanal <fsanal@sas.upenn.edu>
2021-04-14 21:59:11 -0700

    Merging

--- infrastructure/backend/utils/permissions_middleware.js
+++ infrastructure/backend/utils/permissions_middleware.js
@@ -1,12 +1,11 @@
-const Workspace = require('../models/Workspace');
-const Repository = require('../models/Repository');
+const Workspace = require("../models/Workspace");
+const Repository = require("../models/Repository");
 
-var mongoose = require('mongoose');
-const WorkspaceInvite = require('../models/authentication/WorkspaceInvite');
+var mongoose = require("mongoose");
+const WorkspaceInvite = require("../models/authentication/WorkspaceInvite");
 const { ObjectId } = mongoose.Types;
 
 const verifyUserInWorkspace = async (req, res, next) => {
-
     var requestedPath = req.path.trim();
 
     var requesterId = req.tokenPayload.userId.toString();
@@ -15,23 +14,26 @@ const verifyUserInWorkspace = async (req, res, next) => {
     const { workspaceId } = req.params;
     var workspaceFound = false;
     try {
-        console.log(`verifyUserInWorkspace - workspaceId, requesterId: ${workspaceId}, ${requesterId}`);
-        workspaceFound = await Workspace.exists({    _id: ObjectId(workspaceId.toString()),
-                                                    memberUsers: { $in: [ObjectId(requesterId.toString())] }
-                                                });
-    }
-    catch (err) {
+        console.log(
+            `verifyUserInWorkspace - workspaceId, requesterId: ${workspaceId}, ${requesterId}`
+        );
+        workspaceFound = await Workspace.exists({
+            _id: ObjectId(workspaceId.toString()),
+            memberUsers: { $in: [ObjectId(requesterId.toString())] },
+        });
+    } catch (err) {
         return next(new Error(`Error: invalid workspaceId: ${workspaceId}`));
     }
 
     if (workspaceFound) {
         return next();
     }
-    return next(new Error(`Error: no workspace found for workspaceId: ${workspaceId}`));
+    return next(
+        new Error(`Error: no workspace found for workspaceId: ${workspaceId}`)
+    );
 };
 
 const verifyUserAndRepositoryInWorkspace = async (req, res, next) => {
-
     var requestedPath = req.path.trim();
 
     var requesterId = req.tokenPayload.userId.toString();
@@ -40,24 +42,30 @@ const verifyUserAndRepositoryInWorkspace = async (req, res, next) => {
     const { workspaceId, repositoryId } = req.params;
     var workspaceFound = false;
     try {
-        workspaceFound = await Workspace.exists({   _id: ObjectId(workspaceId.toString()),
-                                                    memberUsers: { $in: [ObjectId(requesterId.toString())] },
-                                                    repositories: { $in: [ObjectId(repositoryId.toString())] },
-                                                });
-    }
-    catch (err) {
-        return next(new Error(`Error invalid: workspaceId, repositoryId: ${workspaceId}, ${repositoryId}`));
+        workspaceFound = await Workspace.exists({
+            _id: ObjectId(workspaceId.toString()),
+            memberUsers: { $in: [ObjectId(requesterId.toString())] },
+            repositories: { $in: [ObjectId(repositoryId.toString())] },
+        });
+    } catch (err) {
+        return next(
+            new Error(
+                `Error invalid: workspaceId, repositoryId: ${workspaceId}, ${repositoryId}`
+            )
+        );
     }
 
     if (workspaceFound) {
         return next();
     }
-    return next(new Error(`Error: no workspace found for workspaceId, repositoryId: ${workspaceId}, ${repositoryId}`));
-
-}
+    return next(
+        new Error(
+            `Error: no workspace found for workspaceId, repositoryId: ${workspaceId}, ${repositoryId}`
+        )
+    );
+};
 
 const verifyUserIdMatchesRequester = async (req, res, next) => {
-
     var requestedPath = req.path.trim();
 
     var requesterId = req.tokenPayload.userId.toString();
@@ -73,11 +81,10 @@ const verifyUserIdMatchesRequester = async (req, res, next) => {
     }
 
     return next(new Error(`UserId doesn't match requested Id`));
-
-}
+};
 
 module.exports = {
     verifyUserInWorkspace,
     verifyUserAndRepositoryInWorkspace,
-    verifyUserIdMatchesRequester
-}
+    verifyUserIdMatchesRequester,
+};
