2a30b55bc35e53c13ac638b100d5c1d121596d64
fsanal <fsanal@sas.upenn.edu>
2021-05-04 16:43:03 -0700

    [QD-508] Utilize Text Search to Scope Commits / Pull Requests

--- infrastructure/backend/controllers/pipeline/PipelineController.js
+++ infrastructure/backend/controllers/pipeline/PipelineController.js
@@ -1,5 +1,7 @@
 // models
 const InsertHunk = require("../../models/InsertHunk");
+const Commit = require("../../models/Commit");
+const PullRequest = require("../../models/PullRequest");
 
 //logging
 const { logger } = require("../../fs_logging");
@@ -13,13 +15,37 @@ queryPipeline = async (req, res) => {
     let { query } = req.body;
 
     query = extractQueryKeywords(query);
+
+    const [commits, pullRequests] = searchRepositoryContent(query);
+
+    const hunks = acquireHunks([commits, pullRequests]);
+
+    console.log("Hunks", hunks);
 };
 
 extractQueryKeywords = (query) => {
     return sw.removeStopwords(query, sw.en).join(" ");
 };
 
-searchRepositoryContent = (query) => {};
+searchRepositoryContent = async (query) => {
+    const queries = [Commit, PullRequest].map((model) =>
+        model
+            .find(
+                { $text: { $search: query } },
+                { score: { $meta: "textScore" } }
+            )
+            .sort({ score: { $meta: "textScore" } })
+            .lean()
+    );
+
+    const [commits, pullRequests] = await Promise.all(queries);
+
+    console.log("Commit Output", commits);
+
+    console.log("PullRequest Output", pullRequests);
+
+    return [commits, pullRequests];
+};
 
 acquireHunks = (repositoryContent) => {};
 
