0cc661f95409a50371ba7f23f8b1bdfeed3d0574
fsanal <fsanal@sas.upenn.edu>
2021-05-19 18:29:48 -0700

    Testing CircleCI

---
+++ infrastructure/backend/__tests__/circleci_playground/circleci_playground.spec.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+/*
+// set up mongodb connection
+beforeAll(async () => {
+    const dbRoute = `mongodb+srv://${EXTERNAL_DB_USER}:${EXTERNAL_DB_PASS}@docapp-cluster-hnftq.mongodb.net/test?retryWrites=true&w=majority`;
+
+    await mongoose.connect(dbRoute, { useNewUrlParser: true });
+
+    let db = mongoose.connection;
+
+    db.once("open", () => console.log("connected to the database"));
+
+    db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+    process.env.isTesting = true;
+});
+*/
+
+describe("CircleCI playground to make sure testing is working", () => {
+    test("Sample circle ci test", () => {
+        "Run the test";
+    });
+});
